e60061d07eea3f6d3c24b9f1f246afb377fc9e74
FrancisOwusu <osei1257@gmail.com>
2024-09-17 07:09:29 +0000

    add OAuth Authentication to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const { google } = require('googleapis');
 dotenv.config();
 
 const app = express();
@@ -11,6 +11,35 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//define scope of access for the Google Calender API
+const scopes = ['https://www.googleapis.com/auth/calendar'];
+
+//OAuth 2 configuration
+const oauth2Client = new google.auth.OAuth2
+(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL
+);
+
+app.get('/auth',(req,res)=>{
+  const url = oauth2Client.generateAuthUrl
+  ({
+      access_type: 'offline',
+      scope: scopes
+  });
+  res.redirect(url);
+  }
+);
+
+app.get("/auth/redirect", async (req, res) => {
+
+  const {tokens} = await oauth2Client.getToken(req.query.code);
+  oauth2Client.setCredentials(tokens);
+  res.send('Authentication successful! Please return to the console.');
+  }
+
+);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
